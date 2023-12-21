2744f14fbf0b4c0e5a4a68fd65e3868f5af2ad6d
Tania Ternovaya <ternovaya@live.com>
2023-12-21 16:43:03 +0200

    added

--- innna_test.py
+++ innna_test.py
@@ -1,23 +1,14 @@
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from scipy import stats
 
-# df = pd.concat(
-#     pd.read_excel(r"C:\Users\USR\Downloads\\Test_Yakymenko.xlsx", sheet_name=None),
-#     ignore_index=True,
-# )
-# numeric_columns = df[["Metric Store", "Metric population"]]
-# correlation_matrix = numeric_columns.corr()
-# plt.figure(figsize=(7, 3))
-# sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", linewidths=0.5)
-# plt.title("Correlation Heatmap")
-# plt.xticks(rotation=45)
-# plt.show()
+import seaborn as sns
+import matplotlib.pylab as plt
+fig, ax = plt.subplots(figsize=(4, 4))
 
+norm_sample = stats.norm.rvs(size=100)
+stats.probplot(norm_sample, plot=ax)
 
-def prepare_data(data):
-    x_data = data.sort()
-    return x_data
-
-
-print(prepare_data([2, 34, 5, 6, 7, 8, 1, -90, 23]))
+plt.tight_layout()
+plt.show()
