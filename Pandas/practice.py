531871c5f28f670cbab603118a47e5ed60342bc1
Tania Ternovaya <ternovaya@live.com>
2024-04-09 16:36:33 +0300

    no message

---
+++ Pandas/practice.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+doc = pd.read_csv("/Users/tatanaternovaa/Documents/training-skills/Pandas/Test task_Data processing Assistant_09.06.2023 ternova/task5-Table 1.csv")
+
+
+doc.remove(1)
+print(doc.head())
+
